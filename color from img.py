682e408c5c4c231b40b6b9549fc23daba5a9c127
Sandy <zhuziwen96@gmail.com>
2023-08-04 17:29:25 +0100

    update colour picker

--- color from img.py
+++ color from img.py
@@ -1,6 +1,8 @@
 #https://stackoverflow.com/questions/3241929/python-find-dominant-most-common-color-in-an-image
 
 from colorthief import ColorThief
-color_thief = ColorThief('/path/to/imagefile')
+color_thief = ColorThief('/Users/zhuziwen/Desktop/testImages_artphoto copy/amusement_0487.jpg')
 # get the dominant color
 dominant_color = color_thief.get_color(quality=1)
+
+print(dominant_color)
